80685dc6fe06f110d51b05b3fd66b9d68f16eb08
Mateus <mtsmlima@gmail.com>
2025-04-18 16:47:52 -0300

    Add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Layout } from './pages/Layout'
 import { Home } from './pages/Home'
 import { Menu } from './pages/Menu'
+import { Rewards } from './pages/Rewards'
+import { GiftCards } from './pages/GiftCards'
 import { ErrorPage } from './pages/ErrorPage'
 
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
@@ -10,6 +12,9 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route element={<Layout />}>
     <Route path='/' element={<Home />} />
     <Route path="menu" element={<Menu />} />
+    
+    <Route path="rewards" element={<Rewards />} />
+    <Route path="gift-cards" element={<GiftCards />} />
     <Route path="*" element={<ErrorPage />} />
   </Route>
 ))
